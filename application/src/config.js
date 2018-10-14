a94abd01df2cf77698b7009ee64166ebcb3a93a0
J Kimmell <kimmellj@gmail.com>
2018-10-14 13:28:19 -0400

    Reverted

--- application/src/config.js
+++ application/src/config.js
@@ -9,11 +9,14 @@ const requireProcessEnv = (name) => {
   return process.env[ name ]
 }
 
-const dotenv = require('dotenv-safe')
-dotenv.load({
-  path: path.join(__dirname, '../.env'),
-  sample: path.join(__dirname, '../.env.example')
-})
+/* istanbul ignore next */
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv-safe')
+  dotenv.load({
+    path: path.join(__dirname, '../.env'),
+    sample: path.join(__dirname, '../.env.example')
+  })
+}
 
 const config = {
   all: {
